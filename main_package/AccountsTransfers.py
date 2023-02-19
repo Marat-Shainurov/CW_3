de876ec48c5c7a1eed5b0b93a49eaf3153c58049
Marat-Shainurov <m_shainurov@mail.ru>
2023-02-19 23:35:46 +0300

    template structure of the class in AccountsTransfers.py added

---
+++ main_package/AccountsTransfers.py
@@ -0,0 +1,14 @@
+import requests
+
+
+class AccountsTransfers:
+
+    def __init__(self, path):
+        pass
+
+    def get_data_from_link(self):
+        pass
+
+    def process_array(self, array):
+        pass
+    
