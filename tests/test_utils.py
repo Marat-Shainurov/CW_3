44e74b1c0020f0c28863c436dfe7d7726c34a73f
Marat-Shainurov <m_shainurov@mail.ru>
2023-02-20 00:09:41 +0300

    template structure of test_utils.py added

---
+++ tests/test_utils.py
@@ -0,0 +1,34 @@
+import pytest
+
+
+@pytest.fixture
+def testing_processed_array():
+    return {}
+
+
+def test_sort_processed_array_normal():
+    pass
+
+
+def test_sort_processed_array_sequence():
+    pass
+
+
+def test_get_last_elements_normal():
+    pass
+
+
+def test_convert_data_normal():
+    pass
+
+
+def test_hide_account_sender_normal():
+    pass
+
+
+def test_hide_account_recipient_normal():
+    pass
+
+
+def test_print_results_normal():
+    pass
